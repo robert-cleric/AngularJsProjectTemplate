1b4c53fb5f037dbf2fe16c13db26e272a4508f5d
Robert Cleric <robert.cleric@gmail.com>
2018-03-26 18:10:13 -0400

    Added project.json and other setup files

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "extends": ["angular", "eslint:recommended"],
+    "rules": {
+        "quotes": ["error", "double", {"avoidEscape" : true}],
+        "semi": ["error", "always"]
+    }
+};
